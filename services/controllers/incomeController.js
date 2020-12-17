5a4453f7356d9430f7bc480c15c536a8d5e3f6a2
Dominic Need <Dominicanthonyneed@hotmail.com>
2020-12-17 00:32:16 +0000

    Adding dummy controller for income and routes for income (async npm added)

---
+++ services/controllers/incomeController.js
@@ -0,0 +1,40 @@
+var IncomeInstance = require('../../domain/models/Transaction');
+
+// Display list of all Incomes.
+exports.income_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Income list');
+};
+// Display detail page for a specific Income.
+exports.income_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: income detail: ' + req.params.id);
+};
+
+// Display income create form on GET.
+exports.income_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: income create GET');
+};
+
+// Handle income create on POST.
+exports.income_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: income create POST');
+};
+
+// Display income delete form on GET.
+exports.income_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: income delete GET');
+};
+
+// Handle income delete on POST.
+exports.income_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: income delete POST');
+};
+
+// Display Author update form on GET.
+exports.income_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: income update GET');
+};
+
+// Handle Author update on POST.
+exports.income_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: income update POST');
+};
