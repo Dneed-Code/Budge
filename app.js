dbec1215c357f595d84fabd54a6c36469bc305aa
Dominic Need <Dominicanthonyneed@hotmail.com>
2020-12-27 05:32:29 +0000

    Update controller for income (GET for modal and POST for update)

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv/config');
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = process.env.DB_CONNECTION;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true},() => console.log('connected to Mongo database'));
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false},() => console.log('connected to Mongo database'));
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
