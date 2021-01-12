26bc29905f36ba77f541e2a0c73493f661c14f56
Dominic Need <Dominicanthonyneed@hotmail.com>
2021-01-12 22:03:00 +0000

    env config changed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 var bodyParser = require('body-parser');
 require('toastr');
-require('dotenv/config');
+require('dotenv').config({path: path.join(__dirname, '.env')});
 var moment = require('moment'); // require
 const session = require('express-session');
 const flash = require('connect-flash');
